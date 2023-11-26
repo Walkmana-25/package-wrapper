b667653e745226b6cfd829cb610e9205296428ac
Yuta Takahashi <y.highbridge04@gmail.com>
2023-11-26 17:14:21 +0900

    clap init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli{
+    #[command(Subcommand)]
+    install_Cmd : install
+    
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
 }
